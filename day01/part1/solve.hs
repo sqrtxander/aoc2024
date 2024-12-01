ad3bc783a4ac136d3160af32da68111f6a9849c8
sqrtxander <xanderjames128@gmail.com>
2024-12-01 22:31:14 +1100

    day01 haskell

---
+++ day01/part1/solve.hs
@@ -0,0 +1,22 @@
+import System.IO (readFile)
+import Data.List (sort, span)
+import Data.Char (isSpace)
+
+splitRow :: String -> (String, String)
+splitRow row = let (left, rest) = break isSpace row
+                   right = dropWhile isSpace rest
+               in (left, right)
+
+getNums :: [String] -> ([Int], [Int])
+getNums rows = let (leftStr, rightStr) = unzip $ map splitRow rows
+                   left = map read leftStr
+                   right = map read rightStr
+               in (left, right)
+
+solve :: [String] -> Int
+solve rows = let (left, right) = getNums rows
+             in sum $ zipWith (\a b -> abs(a-b)) (sort left) (sort right)
+
+main = do
+    rows <- lines <$> readFile "input.in"
+    print $ solve rows
---
+++ day01/part2/solve.hs
@@ -0,0 +1,26 @@
+import System.IO (readFile)
+import Data.List (sort, span)
+import Data.Char (isSpace)
+import qualified Data.IntMap as IntMap
+
+splitRow :: String -> (String, String)
+splitRow row = let (left, rest) = break isSpace row
+                   right = dropWhile isSpace rest
+               in (left, right)
+
+getNums :: [String] -> ([Int], [Int])
+getNums rows = let (leftStr, rightStr) = unzip $ map splitRow rows
+                   left = map read leftStr
+                   right = map read rightStr
+               in (left, right)
+
+countOccurrences :: [Int] -> IntMap.IntMap Int
+countOccurrences = foldr (\n acc -> IntMap.insertWith (+) n 1 acc) IntMap.empty
+
+solve :: [String] -> Int
+solve rows = let (left, right) = getNums rows
+                 rightMap = countOccurrences right
+             in sum $ map (\n -> n * IntMap.findWithDefault 0 n rightMap) left
+main = do
+    rows <- lines <$> readFile "input.in"
+    print $ solve rows
