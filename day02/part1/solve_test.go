d53c0a1186b002de2f0344a4db3c828b1b342342
sqrtxander <xanderjames128@gmail.com>
2024-12-02 16:20:56 +1100

    day02 complete in go

---
+++ day02/part1/solve_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+var INPUT string = `
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+`[1:]
+
+var EXPECTED int = 2
+
+func TestSolve(t *testing.T) {
+	actual := solve(INPUT)
+	if actual != EXPECTED {
+		t.Fatalf("Expected %d got %d\n", EXPECTED, actual)
+	}
+}
