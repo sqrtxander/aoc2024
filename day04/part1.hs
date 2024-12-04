da440d366e990ad3e046f87c6f3975f6e54ba116
sqrtxander <xanderjames128@gmail.com>
2024-12-04 19:34:18 +1100

    day04 complete haskell

---
+++ day04/part1.hs
@@ -0,0 +1,37 @@
+import qualified Data.Map as Map
+import System.IO
+
+type Grid = Map.Map (Integer, Integer) Char
+
+parse :: [String] -> Grid
+parse rows = Map.fromList [((x, y), z) | (y, row) <- zip [0 ..] rows, (x, z) <- zip [0 ..] row]
+
+countWord :: String -> (Integer, Integer) -> Grid -> Integer
+countWord s (x, y) g =
+    countWordDirection s (x, y) (0, 1) g
+        + countWordDirection s (x, y) (-1, 1) g
+        + countWordDirection s (x, y) (-1, 0) g
+        + countWordDirection s (x, y) (-1, -1) g
+        + countWordDirection s (x, y) (0, -1) g
+        + countWordDirection s (x, y) (1, 1) g
+        + countWordDirection s (x, y) (1, 0) g
+        + countWordDirection s (x, y) (1, -1) g
+
+countWordDirection :: String -> (Integer, Integer) -> (Integer, Integer) -> Grid -> Integer
+countWordDirection "" _ _ _ = 1
+countWordDirection (z : zs) (x, y) (dx, dy) g =
+    case Map.lookup (x, y) g of
+        Just c | c == z -> countWordDirection zs (x + dx, y + dy) (dx, dy) g
+        _ -> 0
+
+solve :: [String] -> Integer
+solve rows =
+    let
+        grid = parse rows
+    in
+        Map.foldrWithKey (\k _ acc -> acc + countWord "XMAS" k grid) 0 grid
+
+main :: IO ()
+main = do
+    rows <- lines <$> readFile "input.in"
+    print $ solve rows
---
+++ day04/part2.hs
@@ -0,0 +1,47 @@
+import qualified Data.Map as Map
+import Data.Maybe (catMaybes, isJust)
+import System.IO
+
+type Grid = Map.Map (Integer, Integer) Char
+
+parse :: [String] -> Grid
+parse rows = Map.fromList [((x, y), z) | (y, row) <- zip [0 ..] rows, (x, z) <- zip [0 ..] row]
+
+isXMas :: (Integer, Integer) -> Grid -> Bool
+isXMas (x, y) g =
+    case Map.lookup (x, y) g of
+        Just c | c == 'A' -> isRightCount (x, y) g && isRightPermutation (x, y) g
+        _ -> False
+
+isRightCount :: (Integer, Integer) -> Grid -> Bool
+isRightCount (x, y) g =
+    let
+        chars =
+            catMaybes
+                [ Map.lookup (x - 1, y - 1) g,
+                  Map.lookup (x + 1, y + 1) g,
+                  Map.lookup (x - 1, y + 1) g,
+                  Map.lookup (x + 1, y - 1) g
+                ]
+    in
+        length (filter (== 'S') chars) == 2 && length (filter (== 'M') chars) == 2
+
+isRightPermutation :: (Integer, Integer) -> Grid -> Bool
+isRightPermutation (x, y) g =
+    let
+        tl = Map.lookup (x - 1, y - 1) g
+        br = Map.lookup (x + 1, y + 1) g
+    in
+        isJust tl && isJust br && tl /= br
+
+solve :: [String] -> Integer
+solve rows =
+    let
+        grid = parse rows
+    in
+        Map.foldrWithKey (\k _ acc -> acc + if isXMas k grid then 1 else 0) 0 grid
+
+main :: IO ()
+main = do
+    rows <- lines <$> readFile "input.in"
+    print $ solve rows
